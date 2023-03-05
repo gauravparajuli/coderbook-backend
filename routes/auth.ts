7c13d0633a0a8a49ab13bd014edf4e33e71959a6
gaurav parajuli <gparajuli.1997@gmail.com>
2023-03-05 14:25:34 +0545

    login endpoint implemented

--- routes/auth.ts
+++ routes/auth.ts
@@ -2,6 +2,9 @@ import { Router, Request, Response, NextFunction } from 'express'
 import isAuthenticated from '../middlewares/is-authenticated'
 import { userProperty } from '../middlewares/is-authenticated'
 import User from '../models/User'
+import { check, validationResult } from 'express-validator'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
 const router = Router()
 
@@ -18,6 +21,57 @@ router.get(
             res.status(200).json(user)
         } catch (err: any) {
             console.log(err.message)
+            res.status(500).send('Internal server error')
+        }
+    }
+)
+
+// @route      POST api/auth
+// @desc       Authenticate user
+// @access     Public
+router.post(
+    '/',
+    [
+        check('email', 'Email is required').isEmail(),
+        check('password', 'Password is required').exists(),
+    ],
+    async (req: Request, res: Response, next: NextFunction) => {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
+
+        const { email, password } = req.body
+
+        try {
+            // see if user exists
+            let user = await User.findOne({ email })
+            if (!user) {
+                return res
+                    .status(400)
+                    .json({ errors: [{ msg: 'Invalid email or password' }] })
+            }
+
+            // check if password matches
+            if (!(await bcrypt.compare(password, user.password))) {
+                return res
+                    .status(400)
+                    .json({ errors: [{ msg: 'Invalid email or password' }] })
+            }
+
+            // send json web token
+            const payload = {
+                user: {
+                    id: user.id,
+                },
+            }
+            const token = jwt.sign(payload, process.env.JWT_SECRET_KEY!, {
+                expiresIn: 3600 * 24 * 7,
+            })
+
+            res.status(200).json({ token })
+        } catch (err: any) {
+            console.error(err.message)
             res.status(500).send('Internal server error')
         }
     }
