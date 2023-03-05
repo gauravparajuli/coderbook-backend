aedcf6452935e21cc96068da58c663d0d09f2090
gaurav parajuli <gparajuli.1997@gmail.com>
2023-03-05 18:57:29 +0545

    endpoint to fetch currently logged in user profile implemented

--- routes/profile.ts
+++ routes/profile.ts
@@ -1,4 +1,8 @@
 import { Router, Request, Response, NextFunction } from 'express'
+import isAuthenticated from '../middlewares/is-authenticated'
+import { userProperty } from '../middlewares/is-authenticated'
+import Profile from '../models/Profile'
+import User from '../models/User'
 
 const router = Router()
 
@@ -8,5 +12,32 @@ const router = Router()
 router.get('/', (req: Request, res: Response, next: NextFunction) => {
     res.status(200).json({ message: 'profile route working' })
 })
+
+// @route      GET api/profile/i
+// @desc       get currently authenticated user profile
+// @access     Private
+router.get(
+    '/i',
+    isAuthenticated,
+    async (req: Request & userProperty, res: Response, next: NextFunction) => {
+        try {
+            const profile = await Profile.findOne({
+                user: req.user!.id,
+            }).populate('User', ['name', 'avatar'])
+            // console.log(profile)
+
+            if (!profile) {
+                res.status(400).json({
+                    msg: 'There is no profile for this user',
+                })
+            }
+
+            res.status(200).json(profile)
+        } catch (err: any) {
+            console.log(err.message)
+            res.status(500).send('Internal server error')
+        }
+    }
+)
 
 export default router
