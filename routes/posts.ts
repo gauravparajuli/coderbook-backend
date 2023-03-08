9d0f2679f1bdc6ac45b0ec97a17e14afa1ca4caf
gaurav parajuli <gparajuli.1997@gmail.com>
2023-03-09 05:26:36 +0545

    implemented endpoint to create post

--- routes/posts.ts
+++ routes/posts.ts
@@ -1,12 +1,45 @@
 import { Router, Request, Response, NextFunction } from 'express'
+import isAuthenticated from '../middlewares/is-authenticated'
+import { userProperty } from '../middlewares/is-authenticated'
+import { check, validationResult } from 'express-validator'
+import Post from '../models/Post'
+import Profile from '../models/Profile'
+import User from '../models/User'
 
 const router = Router()
 
-// @route      GET api/posts
-// @desc       Test route
-// @access     Public
-router.get('/', (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).json({ message: 'posts route working' })
-})
+// @route      POST api/posts
+// @desc       Create new post
+// @access     Private
+router.post(
+    '/',
+    isAuthenticated,
+    [check('text', 'Text is required').not().isEmpty()],
+    async (req: Request & userProperty, res: Response, next: NextFunction) => {
+        try {
+            const errors = validationResult(req)
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() })
+            }
+
+            const user = await User.findById(req.user!.id).select('-password')
+
+            if (!user) return res.status(404).json({ msg: 'User not found' })
+
+            const newPost = new Post({
+                text: req.body.text,
+                name: user.name,
+                gravatar: user.gravatar,
+                user: req.user!.id,
+            })
+
+            await newPost.save()
+            res.status(201).json(newPost)
+        } catch (err: any) {
+            console.log(err.message)
+            res.status(500).send('Internal server error')
+        }
+    }
+)
 
 export default router
