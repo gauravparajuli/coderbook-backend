c14bb50306896c3be8bb1890b0ed90fe93fdcae7
gaurav parajuli <gparajuli.1997@gmail.com>
2023-03-08 10:21:13 +0545

    implemented post schema

---
+++ models/Post.ts
@@ -0,0 +1,55 @@
+import { Schema, model } from 'mongoose'
+
+const postSchema = new Schema(
+    {
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            required: true,
+        },
+        text: {
+            type: String,
+            required: true,
+        },
+        name: {
+            type: String,
+        },
+        gravatar: {
+            type: String,
+        },
+        likes: [
+            {
+                user: {
+                    type: Schema.Types.ObjectId,
+                    ref: 'User',
+                    unique: true,
+                },
+            },
+        ],
+        comments: [
+            {
+                user: {
+                    type: Schema.Types.ObjectId,
+                    ref: 'User',
+                },
+                text: {
+                    type: String,
+                    required: true,
+                },
+                name: {
+                    type: String,
+                },
+                gravatar: {
+                    type: String,
+                },
+                date: {
+                    type: Date,
+                    deafult: Date.now(),
+                },
+            },
+        ],
+    },
+    { timestamps: true }
+)
+
+export default model('Post', postSchema)
